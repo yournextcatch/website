3ff1256e3de39440f6359919f5a082ae111776af
Richard Mundy III <mundy.richard@gmail.com>
2025-05-22 20:51:08 -0700

    real HB fetch

--- src/components/HotBites/HotBites.jsx
+++ src/components/HotBites/HotBites.jsx
@@ -1,39 +1,28 @@
 import { useState, useEffect } from "react";
 import BiteCard from "../BiteCard/BiteCard";
 import { Container } from "react-bootstrap";
-
-const hotData = [
-  {
-    title: "7 Yellowtail Reported",
-    article:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi veniam amet obcaecati eligendi, repellendus impedit nihil optio, odit necessitatibus delectus in ipsum voluptate eum, nulla minima suscipit temporibus quisquam rem. Iure neque nam necessitatibus cumque recusandae nulla voluptate ut quod alias aut sint laborum optio reiciendis incidunt ex quam reprehenderit dicta, officia sapiente eveniet officiis illum dolor? Rem, deserunt repellendus",
-  },
-  {
-    title: "Limits Reached",
-    article:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit architecto, ut corporis quo expedita assumen aperiam dicta quis obcaecati numquam labore ipsum eveniet totam nobis soluta. Quae sunt nisi sequi?",
-  },
-];
+import * as HotBitesApi from "../../utilities/api/HotBitesApi";
 
 function HotBites() {
   const [hotBites, setHotBites] = useState([]);
 
   useEffect(() => {
-    // Simulate an API call to fetch hot bites data
-    const fetchHotBites = () => {
-      // Simulating a delay with setTimeout
-      setTimeout(() => {
-        setHotBites(hotData);
-      }, 1000);
-    };
+    async function getAllBites() {
+      const hotBites = await HotBites.getAllBites();
 
-    fetchHotBites();
+      setHotBites(hotBites);
+    }
   }, []);
+
   return (
     <>
       <Container style={{ display: "flex", flexDirection: "column" }}>
         {hotBites.map((bite, index) => (
-          <BiteCard key={index} title={bite.title} article={bite.article} />
+          <BiteCard
+            key={index}
+            title={bite.title}
+            article={bite.article.slice(0, 50)}
+          />
         ))}
       </Container>
     </>
